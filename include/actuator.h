bca7af7a32d0fcdff324f888d408c0865d9d99a4
Victor <victor.camargo@rappi.com>
2019-02-14 02:26:11 -0500

    5.1 - Overload

--- include/actuator.h
+++ include/actuator.h
@@ -6,4 +6,22 @@ public:
   Actuator(String name, String type, uint16_t id = random(0, 0xFFFF)): Peripheal(name, id) {
       this->type = type;
   }
+  String getType();
+  void report(Stream &uart);
 };
+
+String Actuator::getType(){
+  return this->type;
+};
+
+void Actuator::report(Stream &uart){
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& res = jsonBuffer.createObject();
+    res["name"] = this->getName();
+    res["id"] = this->getId();
+    res["type"] = "Actuator";
+    res["act_type"] = this->getType();
+    String out;
+    res.prettyPrintTo(out);
+    uart.println(out);
+}
