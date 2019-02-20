03140316502b2dc2c6c4f45ed1cc1a6f2ffc0a25
manuquirozy <manuquirozy@gmail.com>
2019-02-20 18:20:12 -0500

    Comentarios listos

--- src/functions.h
+++ src/functions.h
@@ -1,16 +1,24 @@
-#ifndef function_h
+#ifndef function_h //si cuando se está compilando el programa function_h no está definida entonces la define
 #define function_h
 
+
 #include <Arduino.h>
+//incluye la librería de Arduino
 
 float readTemp(int var){
-  return (float(analogRead(0)/1023.0)*5.0);
+  /*función que lee el puerto análogo 0 y retorna su valor
+  remplacé el 0 por la variable var
+  return (float(analogRead(0)/1023.0)*5.0); */
+  return (float(analogRead(var)/1023.0)*5.0);
 }
 
 bool setSpeed(uint16_t duty){
-  char buff[50];
-  sprintf(buff, "PWM set to %2d");
-  Serial.print(buff);
+  //recibe el duty cicle que debe tener el PWM que controla la velocidad del motor
+  //debería retornar true o false. no se está usando duty
+  char buff[50]; //crea una variable tipo char llamada buff con 50 caracteres
+  sprintf(buff, "PWM set to %2d", duty); //guarda en buff (el buffer) "PWM set to $duty$"
+  Serial.print(buff); //escribe en el puerto serial lo que hay en buff
+  return true; //se agregó porque no se estaba retornando nada
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,24 @@
 #include <sensor.h>
 #include <actuator.h>
 #include "functions.h"
-
+//header. Incluye las librerías, las clases y los archivos extra que necesita.
 
 Sensor sensor = Sensor(String("lm35"), String("°C"));
+//crea un objeto sensor de la clase Sensor con name=lm35 y units=°C. El id será aleatorio
 Actuator actuator = Actuator(String("AX500"), String("Speed Controller"));
+//crea un objeto actuator de la clase Actuator con name=AX500 y type=Speed Controller. EL id será aleatorio
 
 void setup() {
-  Serial.begin(115200);
-  sensor.attach(readTemp);
-  actuator.attach(setSpeed);
+  Serial.begin(115200); //inicializa el puerto serial con un baud rate de 115200 baudios
+  sensor.attach(readTemp); //le adjunta a sensor la función readTemp
+  actuator.attach(setSpeed); //le adjunta a actuator la función setSpeed
 }
 
 void loop() {
-  Serial.println(sensor.run(0));
-  actuator.report(Serial);
+  Serial.println(sensor.run(0)); //escribe por el puerto serial el resultado de ejecutar la función readTemp con un argumento 0
+  actuator.report(Serial); //escribe por el puerto serial name, id, "Actuator" y type del objeto actuator
   while(1){
-
+    //este ciclo se repite infinitamente. Se hace para que solo se ejecute una vez lo del loop
   }
 }
 
